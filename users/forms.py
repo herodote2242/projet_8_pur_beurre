2b78790c81b8c58dd04e3458d51ebf503b9e92f4
unknown <matthias.gil@hotmail.fr>
2019-05-17 15:57:45 +0200

    registerform added

--- users/forms.py
+++ users/forms.py
@@ -10,3 +10,23 @@ class ConnexionForm(forms.Form):
     password = forms.CharField(label="Mot de passe",
                                widget=forms.PasswordInput)
 
+
+class SearchForm(forms.Form):
+    """
+    This class creates the form used by the visitor to search a product.
+    """
+    product_to_search = forms.CharField(label="Rechercher un produit", max_length=100)
+
+class SignUpForm(forms.Form):
+    """
+    With this class, a visitor can register his profile in the database. It
+    allows the visitor to be a member, to log in and log out, and of course
+    to be able to save favorites.
+    """
+    username = forms.CharField(label="Nom d'utilisateur", max_length=30)
+    email = forms.EmailField(label="Adresse mail", widgets=forms.EmailInput)    
+    password = forms.CharField(label="Mot de passe",
+        widget=forms.PasswordInput)
+    check_password = forms.CharField(
+        label="Vérification du mot de passe saisi",
+        widget=forms.PasswordInput)
