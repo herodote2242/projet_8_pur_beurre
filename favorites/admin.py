f7f6996afe6eae995435767dcc6757a463ffbc68
unknown <matthias.gil@hotmail.fr>
2019-05-28 14:59:50 +0200

    adding models to the admin tool

--- favorites/admin.py
+++ favorites/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Favorite
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+   model = Favorite
+   verbose_name = 'Favoris enregistrés'
+
--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Product
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
    model = Product
-   verbose_name = 'Produit Sauvegardé'
+   verbose_name = 'Produits disponibles'
